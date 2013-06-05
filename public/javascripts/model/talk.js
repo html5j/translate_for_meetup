e5ef3c4ab53c38bf82ef433461fb69200181dba1
kensakukomatsu <kensaku.komatsu@gmail.com>
2013-06-05 15:34:47 +0900

    for issue #17 any device shows same timestamp format

--- public/javascripts/model/talk.js
+++ public/javascripts/model/talk.js
@@ -1,49 +1,57 @@
 /**
  * Talk
  */
+var Talk;
+(function(){
+	var getTimestamp_ = function(){
+		var d = new Date()
+			,ret = "";
 
-var Talk = function(obj){
-	// [todo] 英語とか日本語とか自動翻訳結果とかをmodeで指定して、rederingの結果に
-	// に反映させるようにするかもしれない
-	this.id = obj.id; this.name = obj.name; this.ja = obj.ja; this.en = obj.en; this.fix = obj.fix;
-}
+		ret = d.getFullYear()+"/"+(d.getMonth() + 1)+"/"+d.getDate()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()
 
-Talk.prototype.render = function(){
-	var template_trans_ = [
-		"<dt>${name}</dt>",
-		"<dd>",
-		"<p class='ja'>${ja}</p>",
-		"<p class='en'>${en}</p>",
-		"<p><time>${timestamp}</time></p>",
-		"</dd>"
-	].join("")
-
-	var template_fix_ = [
-		"<dt>${name}</dt>",
-		"<dd>",
-		"<p class='fix'>${fix}</p>",
-		"<p><time>${timestamp}</time></p>",
-		"</dd>"
-	].join("")
-
-	var timestamp_ = new Date().toLocaleString(); 
-
-	if(!!this.fix) {
-		return template_fix_
-			.replace("${name}", this.name)
-			.replace("${fix}", this.fix)
-			.replace("${timestamp}", timestamp_)
-	} else if(!!this.ja || !!this.en) {
-		var en_ = !!this.en ? this.en : "....."
-			, ja_ = !!this.ja ? this.ja : "。。。。。"
-			
-		return template_trans_
-			.replace("${name}", this.name)
-			.replace("${ja}", ja_)
-			.replace("${en}", en_)
-			.replace("${timestamp}", timestamp_)
-	} else {
-		return "";
+		return ret;
 	}
-}
 
+	Talk = function(obj){
+		this.id = obj.id; this.name = obj.name; this.ja = obj.ja; this.en = obj.en; this.fix = obj.fix;
+	}
+
+	Talk.prototype.render = function(){
+		var template_trans_ = [
+			"<dt>${name}</dt>",
+			"<dd>",
+			"<p class='ja'>${ja}</p>",
+			"<p class='en'>${en}</p>",
+			"<p><time>${timestamp}</time></p>",
+			"</dd>"
+		].join("")
+
+		var template_fix_ = [
+			"<dt>${name}</dt>",
+			"<dd>",
+			"<p class='fix'>${fix}</p>",
+			"<p><time>${timestamp}</time></p>",
+			"</dd>"
+		].join("")
+
+		var timestamp_ = getTimestamp_(); 
+
+		if(!!this.fix) {
+			return template_fix_
+				.replace("${name}", this.name)
+				.replace("${fix}", this.fix)
+				.replace("${timestamp}", timestamp_)
+		} else if(!!this.ja || !!this.en) {
+			var en_ = !!this.en ? this.en : "....."
+				, ja_ = !!this.ja ? this.ja : "。。。。。"
+
+			return template_trans_
+				.replace("${name}", this.name)
+				.replace("${ja}", ja_)
+				.replace("${en}", en_)
+				.replace("${timestamp}", timestamp_)
+		} else {
+			return "";
+		}
+	}
+}());
